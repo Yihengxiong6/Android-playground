a33d32906806d30b71b5623fa01c8bcfc9e88861
georgexyh <xiongyiheng1996@gmail.com>
2020-08-30 01:13:09 -0400

    clicklistener

--- app/src/main/java/com/yihengx/myapplication/MainActivity.java
+++ app/src/main/java/com/yihengx/myapplication/MainActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.PriorityQueue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,37 +22,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "We are at onCreate()");
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "We are at onStart()");
-    }
+        final TextView welcomeTextView = findViewById(R.id.welcomeTextView);
+        final EditText nameEditText = findViewById(R.id.nameEditText);
+        final EditText emailEditText = findViewById(R.id.emailEditText);
+        Button submitButton = findViewById(R.id.submitButton);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "We are at onResume()");
-    }
+        // 可以和comparator一样理解
+        // 因为java里无法用callback，
+        // 所以只能在arguments里传一个interface然后定义要用函数的abstract
+        // 比如这里，button的 setOnclicklistener 里需要有一个click后的一些行为的逻辑，比如对现在view的改变
+        // 这些逻辑只能从当前的class传入到setOnclickListener, 所以就用了一个interface的办法
+        // 事先定义好这个interface，和代表这个行为逻辑的abstract方法，这样就可以在setOnClickerListener
+        // 里用一个变量接住这个interface，然后调用该方法（逻辑）
+        //
+        submitButton.setOnClickListener(new Button.OnClickListener() {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "We are at onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "We are at onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "We are at onDestroy()");
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                welcomeTextView.setText("Welcome " + name + ", your email is: " + email);
+            }
+        });
     }
 }
 
